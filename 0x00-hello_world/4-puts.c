38f2d1b320ac8ed8be9bdebe66a503f45cb56c52
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-08 17:56:32 -0700

    create a c programme using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ * Write a C program that prints exactly
+ *  "Programming is like building a multilingual puzzle", followed by a new line.
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
