89c12ec4bce1763e0844c641db420b5b1ef67692
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-09-09 02:02:12 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 /*
  * Write a C program that prints exactly
- *  "Programming is like building a multilingual puzzle", followed by a new line.
+ * "Programming is like building a multilingual puzzle", followed by a new line.
  */
-
 
 #include <stdio.h>
 
+/**
+ * main - main block
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
