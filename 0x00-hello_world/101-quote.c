f51cc840be57b8fcf5761f7d7b65c5ad15d679ce
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-09-09 03:19:28 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	const char s = "and that piece of art is useful\" - Dora Korpar,2015-10-19\n";
+	int s = strlen("and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
 
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      strlen(s));
+	      s);
 	return (1);
 }
