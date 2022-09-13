eb026d47097b0eab100409fcb630929d6961e95b
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-13 15:40:12 -0700

    create 0-putchar.c programme file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions prototypes */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +18,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif /* HEADER_H */
