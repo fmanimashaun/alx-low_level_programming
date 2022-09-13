e672e2fba67408b454000f60423bc3d82e90f403
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-13 16:10:52 -0700

    create 2-print_-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase 10x using only `_puutchar`
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
