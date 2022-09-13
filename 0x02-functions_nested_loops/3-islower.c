c1684972dc60f5fcbfb177d90df4fb5b75d971fb
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-09-14 00:45:10 +0100

    updated 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
  * _islower - checks if a character is lowercase using only `_putchar`
+ * 
+ * @c: the character  to check
  * Return: 0
  */
 
 int _islower(int c)
 {
 	if ((c >= 97) && (c <= 122))
-	{
-		return 1;
-	}
+		return (1);
 	else
-	{
-		return 0;
-	}
+		return (0);
 			
 }
