05ada2fe938d725aa9b35467d77f165bd56c388f
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-13 17:18:17 -0700

    Create a 5-sign.c function file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * using only `_putchar`
+ * @n: the the number to check for sign
+ *
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n >  0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	else
+	{
+		_putchar("-");
+		return (0);
+	}
+}
