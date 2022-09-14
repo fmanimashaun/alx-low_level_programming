5b762870d221261fd9e355c39e668660515fb119
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-13 17:04:32 -0700

    create 4-alpha.c function file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is alphabetic character using only `_putchar`
+ * @c: the character to check
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+		return (1);
+	else
+		return (0);
+}
