c0426aa44c2d206cbf0751c6a78d067b7d642f5b
fmanimashaun <fisayo.animashaun@outlook.com>
2022-09-15 20:48:11 -0700

    fixed the the code error in 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
 * Return: void
 */
 
-void print_remaining_day(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
@@ -19,7 +19,17 @@ void print_remaining_day(int month, int day, int year)
 			day++;
 		}
 
-		printf("Day of the year: %day\n", day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
+
+		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
